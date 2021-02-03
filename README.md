2cb75b156d88536955d3dd9614333a7265928167
Dan- <misternutel@gmail.com>
2021-02-02 22:28:03 -0500

    Tasks are added to the window

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Gtk-todo
+
 A todo application created with Gtk 
+
+**This application was created to learn the basics of GTK** 
+
+# How to run?
+
+First you need to install Gtk. You can see an installation guide [here](https://pygobject.readthedocs.io/en/latest/getting_started.html#ubuntu-getting-started)
+
+Now you just have to run the python file
+
+`python3 app.py`
--- app.py
+++ app.py
@@ -8,25 +8,32 @@ class Window(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self, title="ToDo")
         
-        main_box = Gtk.Box(spacing=6)
-        self.add(main_box)
-
+        self.main_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        self.add(self.main_box)
+        
+        fbox = Gtk.Box(spacing=6)
+        self.main_box.pack_start(fbox, True, True, 10)
+        
         # Entry and labels
-        input_title = Gtk.Label(label="task")
+        input_title = Gtk.Label(label="Task")
         self.add_button = Gtk.Button(label="Add")
         self.add_button.connect("clicked", self.add_todo)
         self.input = Gtk.Entry()
         self.input.connect("activate", self.add_todo)
         
         # add widgets 
-        main_box.pack_start(input_title, True, True, 7)
-        main_box.pack_start(self.input, True, True, 0)
-        main_box.pack_start(self.add_button, True, False, 7)
+        fbox.pack_start(input_title, True, True, 7)
+        fbox.pack_start(self.input, True, True, 0)
+        fbox.pack_start(self.add_button, True, False, 7)
         
         
     def add_todo(self, widget):
-        print(self.input.get_text())
-        
+        if self.input.get_text():
+            new_task = Gtk.Label(label=self.input.get_text())
+            self.main_box.pack_start(new_task, True, True, 0)
+            self.main_box.show_all()
+            self.input.set_text('')
+
 if __name__ ==  '__main__':
     window = Window()
     window.connect("destroy", Gtk.main_quit)
